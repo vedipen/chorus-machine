fca001df15e3ece5247b3643a4caaa8e674228b3
Dipen Ved <vedipen@gmail.com>
2017-06-18 17:20:25 +0530

    Add setting menu

--- main.js
+++ main.js
@@ -36,3 +36,34 @@ ipc.on('close-main-window', function () {
 });
 
 
+
+
+var settingsWindow = null;
+
+ipc.on('open-settings-window', function () {
+    if (settingsWindow) {
+        return;
+    }
+
+    settingsWindow = new BrowserWindow({
+        frame: false,
+        height: 200,
+        resizable: false,
+        width: 200
+    });
+
+    settingsWindow.loadUrl('file://' + __dirname + '/app/settings.html');
+
+    settingsWindow.on('closed', function () {
+        settingsWindow = null;
+    });
+});ipc.on('close-settings-window', function () {
+    if (settingsWindow) {
+        settingsWindow.close();
+    }
+});
+ipc.on('close-settings-window', function () {
+    if (settingsWindow) {
+        settingsWindow.close();
+    }
+});
