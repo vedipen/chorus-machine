2557ad76183e3bcc9fd4bb458ab8b931c8d3e61a
Dipen Ved <vedipen@gmail.com>
2017-06-18 18:40:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,16 @@
 
 var app = require('app');
 var BrowserWindow = require('browser-window');
+var configuration = require('./configuration');
 
 var mainWindow = null;
 
 app.on('ready', function() {
+    
+    if (!configuration.readSettings('shortcutKeys')) {
+        configuration.saveSettings('shortcutKeys', ['ctrl', 'shift']);
+    }
+    
     mainWindow = new BrowserWindow({
         frame: false,
         height: 700,
@@ -15,18 +21,7 @@ app.on('ready', function() {
     });
 
     mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
-    
-    var globalShortcut = require('global-shortcut');
-    globalShortcut.register('ctrl+shift+1', function () {
-            mainWindow.webContents.send('global-shortcut', 0);
-    });
-    globalShortcut.register('ctrl+shift+2', function () {
-        mainWindow.webContents.send('global-shortcut', 1);
-    });
-    globalShortcut.register('space', function () {
-        var random = Math.floor((Math.random() * 2) + 1);
-        mainWindow.webContents.send('global-shortcut', random);
-    });
+    setGlobalShortcuts(); 
 });
 
 var ipc = require('ipc');
@@ -35,6 +30,20 @@ ipc.on('close-main-window', function () {
     app.quit();
 });
 
+function setGlobalShortcuts() {
+    globalShortcut.unregisterAll();
+
+    var shortcutKeysSetting = configuration.readSettings('shortcutKeys');
+    var shortcutPrefix = shortcutKeysSetting.length === 0 ? '' : shortcutKeysSetting.join('+') + '+';
+    
+    globalShortcut.register(shortcutPrefix + '1', function () {
+        mainWindow.webContents.send('global-shortcut', 0);
+    });
+    
+    globalShortcut.register(shortcutPrefix + '2', function () {
+        mainWindow.webContents.send('global-shortcut', 1);
+    });
+}
 
 
 
